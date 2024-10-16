687c6136a4d704dc4e4e50fe5cd300fb05fc4d36
Dark Waves Tech <89140466+darkwaves-ofc@users.noreply.github.com>
2024-10-17 01:25:49 +0530

    u

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Particle from "./components/Particle";
 import Content from "./Content";
-
 function App() {
   return (
-    <div className="font-raleway">
+    <div className="font-raleway relative">
       <Content />
-      <Particle />
+      {/* <Particle /> */}
+      <img
+        src={"/heartstopper/bg.gif"}
+        className="absolute top-0 left-0 w-full h-full -z-10"
+      />
     </div>
   );
 }
